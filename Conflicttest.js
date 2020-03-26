178213bf9556847940d0a8af5bfd901066942915
vibano <ndvibanpro@gmail.com>
2020-03-26 02:31:48 -0400

    now

--- Conflicttest.js
+++ Conflicttest.js
@@ -1,4 +1,4 @@
 I want to make sure all is OK with you guys
 Please do all to make things great... as great things do not come easy
 Always was your hands with running water
-Great talk for sure
+Great talk for sure...for sure i will follow up
